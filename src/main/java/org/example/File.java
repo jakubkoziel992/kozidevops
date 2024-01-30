8770ff0261881a24aa17bd2f5f8a8de4464f2f93
kozijak <kozijak@op.pl>
2024-01-30 15:52:30 +0100

    added logic

--- src/main/java/org/example/File.java
+++ src/main/java/org/example/File.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class File {
+    private final String name;
+
+    public File(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
