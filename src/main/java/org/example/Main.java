69952f4b1d8d786ec5c05edcccfe5ec22297ad21
kozijak <kozijak@op.pl>
2024-01-30 13:58:19 +0100

    the method of the Monitor class have been validated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Monitor monitor = new Monitor("Dell");
+        System.out.println(monitor.getResolution());
+
+        monitor.setLowResolution();
+        System.out.println(monitor.getResolution());
     }
 }
