4f9608ac7b063c954efa179d5af4a5cd3a218fd7
kozijak <kozijak@op.pl>
2024-01-30 13:53:35 +0100

    added logic to Monitor class

--- src/main/java/org/example/Monitor.java
+++ src/main/java/org/example/Monitor.java
@@ -1,4 +1,28 @@
 package org.example;
 
 public class Monitor {
+    private final String name;
+    private int width = 3840;
+    private int height = 1920;
+
+    public Monitor(String name) {
+        this.name = name;
+    }
+
+    public void setHightResolution(){
+        width = 3840;
+        height = 1920;
+    }
+
+    public void setLowResolution(){
+        width = 800;
+        height = 600;
+    }
+
+    public String getResolution(){
+        return width + "x" + height;
+    }
+
+
+
 }
