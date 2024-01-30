3564c17c26c1e1f6efc91d67fbcbe5ef31e24013
kozijak <kozijak@op.pl>
2024-01-30 14:19:20 +0100

    class Drive - work has begun

--- src/main/java/org/example/Drive.java
+++ src/main/java/org/example/Drive.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Drive {
+    List<File> hddFiles = new ArrayList<>();
+    Map<String,File> ssdFiles = new HashMap<>();
+    private final String type;
+
+    public Drive(String type) {
+        this.type = type;
+    }
 }
