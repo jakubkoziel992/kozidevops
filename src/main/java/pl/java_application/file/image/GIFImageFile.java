51df4d517418bc0d82b41f4060268f7baf2c3d4d
kozijak <kozijak@op.pl>
2024-01-31 12:36:24 +0100

    adjusted classes

--- src/main/java/pl/java_application/file/image/GIFImageFile.java
+++ src/main/java/pl/java_application/file/image/GIFImageFile.java
@@ -1,31 +1,12 @@
 package pl.java_application.file.image;
 
-import pl.java_application.file.File;
-import pl.java_application.file.FileType;
 
-public class GIFImageFile implements File {
-    private final String name;
-    private final int size;
+public class GIFImageFile extends AbstractImageFile {
 
     public GIFImageFile(String name, int size) {
-        this.name = name;
-        this.size = size;
+       super(name,size);
     }
 
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public int getSize() {
-        return size;
-    }
-
-    @Override
-    public FileType getType() {
-        return FileType.IMAGE;
-    }
 
     public void showAnimation(){
         System.out.println("Showing funny GIF");
--- src/main/java/pl/java_application/file/image/JPGImageFile.java
+++ src/main/java/pl/java_application/file/image/JPGImageFile.java
@@ -1,32 +1,11 @@
 package pl.java_application.file.image;
 
-import pl.java_application.file.File;
-import pl.java_application.file.FileType;
-
-public class JPGImageFile implements File {
-    private final String name;
-    private final int size;
+public class JPGImageFile extends AbstractImageFile {
     private final int compression;
-    
+
     public JPGImageFile(String name, int size, int compression) {
-        this.name = name;
-        this.size = size;
+        super(name,size);
         this.compression = compression;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public int getSize() {
-        return size;
-    }
-
-    @Override
-    public FileType getType() {
-        return FileType.IMAGE;
     }
 
     public int getCompression() {
