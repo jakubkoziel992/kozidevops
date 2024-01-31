5b79ccbc5307842c2f5addc94736fc21617643c8
kozijak <kozijak@op.pl>
2024-01-31 11:53:15 +0100

    checked if new logic works correctly

--- src/main/java/pl/java_application/Main.java
+++ src/main/java/pl/java_application/Main.java
@@ -1,6 +1,11 @@
 package pl.java_application;
 
 import pl.java_application.drive.SSDDrive;
+import pl.java_application.usbdevice.MemoryStick;
+import pl.java_application.usbdevice.Mouse;
+import pl.java_application.usbdevice.USBDevice;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +14,28 @@ public class Main {
 
         Computer computer = new Computer(monitor, ssdDrive);
 
-        computer.addFile(new File("file.png"));
-        computer.listFiles();
+        //computer.addFile(new File("file.png"));
+        //computer.listFiles();
+
+        Mouse mouse = new Mouse("Mysz");
+        MemoryStick memoryStick = new MemoryStick("Pendrive");
+
+        computer.addUSBDevice(mouse);
+        computer.addUSBDevice(memoryStick);
+
+        computer.removeUSBDevice(mouse);
+/*
+        List<USBDevice> usbDevices = computer.getUsbDevices();
+
+        for (USBDevice device: usbDevices){
+            System.out.println(device.getName());
+        }
+*/
+        memoryStick.eject();
+        computer.removeUSBDevice(memoryStick);
+
+
+
+
     }
 }
